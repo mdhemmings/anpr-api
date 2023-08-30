24d3703579344fc58bba5c6263bb7adedbdbd840
itechMDHemmings <matt.hemmings@itech.media>
2023-08-30 11:42:55 +0100

    working apitoken

--- cmd/auth/middleware.go
+++ cmd/auth/middleware.go
@@ -19,7 +19,6 @@ func AuthMiddleware(next http.Handler) http.Handler {
 
 		token := strings.TrimPrefix(authorizationHeader, "Bearer ")
 
-		// Validate the token (you can implement your own token validation logic here)
 		if token != os.Getenv("APIKEY") {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			log.Println("Invalid bearer token")
@@ -31,18 +30,15 @@ func AuthMiddleware(next http.Handler) http.Handler {
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Set CORS headers for all requests
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Replace "*" with your desired origin(s)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		// Handle preflight OPTIONS requests
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 
-		// Continue processing the request
 		next.ServeHTTP(w, r)
 	})
 }
